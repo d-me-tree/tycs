184c3dafdd5422e3cfbe3d0625d0daf339b95573
Dimitri Denisjonok <dimitri.781@gmail.com>
2017-07-25 13:13:29 +0100

    Add completed hw3

--- 01-programming/cs61as/hw3.rkt
+++ 01-programming/cs61as/hw3.rkt
@@ -6,16 +6,16 @@
 ; Exercise 1 - Define fast-expt-iter
 
 (define (fast-expt-iter b n)
-  ; Your code here
-  (error "Not yet implemented")
-)
+  (define (iter a b n)
+    (cond [(= n 0) a]
+          [(even? n) (iter a (square b) (/ n 2))]
+          [else (iter (* a b) b (- n 1))]))
+  (iter 1 b n))
 
 ; Exericse 2 - Define phi
 
 (define (phi)
-  ; Your code here
-  (error "Not yet implemented")
-)
+  (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
 
 (define tolerance 0.00001)
 
@@ -32,34 +32,57 @@
 ; Exercise 3 - Define cont-frac
 
 ;; Recursive version
+
+;(define (cont-frac n d k)
+;  (cond
+;    [(= k 0) 0]
+;    [(/ (n k) (+ (d k) (cont-frac n d (- k 1))))]))
+; INCORRECT: produces Nk / (Dk + ...) vs N1 / (D1 + ...)
+
 (define (cont-frac n d k)
-  ; Your code here
-  (error "Not yet implemented")
-)
+  (define (recur i)
+    (if (> i k)
+        0
+        (/ (n i) (+ (d i) (recur (+ i 1))))))
+  (recur 1))
+
 
 ;; Iterative version
 (define (cont-frac-iter n d k)
-  ; Your code here
-  (error "Not yet implemented")
-)
+  (define (iter result i)
+    (if (= 0 i)
+        result
+        (iter (/ (n i) (+ (d i) result)) (- i 1))))
+  (iter 0 k))
 
 (define (e k)
-  ; Your code here to estimate e using cont-frac with k terms.
-  (error "Not yet implemented")
-)
+  (+ 2 (cont-frac (lambda (i) 1.0)
+                  (lambda (i)
+                    (if (= (remainder i 3) 2)
+                        (/ (+ i 1) 1.5)
+                        1))
+                  k)))
 
 ; Exercise 4 - Define next-perf
 
 (define (next-perf n)
-  ;  Your code here
-  (error "Not yet implemented")
-)
+  (define (sum-of-factors k n)
+    (cond [(= k 0) 0]
+          [(= (remainder n k) 0) (+ k (sum-of-factors (- k 1) n))]
+          [(sum-of-factors (- k 1) n)]))
+  (define (is-perfect? n)
+    (and (> n 0) (= (sum-of-factors (- n 1) n) n)))
+  (if (is-perfect? n)
+      n
+      (next-perf (+ n 1))))
+  
 
 ; Exercise 5 - Explain what happens when the base cases are interchanged.
 
 #|
 
-Your explanation here
+(cc 0 '()) -- > 0, which is incorrect
+we want to count 0 as 1 way to make change
 
 |#
 
@@ -70,5 +93,6 @@ Your explanation here
 Formula for expt:
 
 Formula for expt-iter:
+product * b ^ counter == b ^ n
 
 |#
